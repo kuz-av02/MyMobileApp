68422fd048887fbf7ddfe4fe3f5b3fb3eb09a677
kuz-av02 <kuz-av02@mail.ru>
2024-04-18 21:55:50 +0300

    add MyImage layout

--- app/src/main/java/com/example/myapplication/MyImage.kt
+++ app/src/main/java/com/example/myapplication/MyImage.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +11,8 @@ import androidx.core.view.WindowInsetsCompat
 class MyImage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.myimage)
+        val choosebtn =findViewById<Button>(R.id.choosebtn)
+        choosebtn.setOnClickListener { startActivity(Intent(this@MyImage, Profile::class.java)) }
     }
 }
--- app/src/main/java/com/example/myapplication/Profile.kt
+++ app/src/main/java/com/example/myapplication/Profile.kt
@@ -2,9 +2,11 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,7 +18,28 @@ class Profile : AppCompatActivity() {
         val second_btn =findViewById<Button>(R.id.change_language)
         val third_btn =findViewById<Button>(R.id.change_image)
         val fourth_btn =findViewById<Button>(R.id.Logout)
-        first_btn.setOnClickListener { startActivity(Intent(this@Profile, MainActivity::class.java)) }
+
+        first_btn.setOnClickListener {
+            // Получение текущей темы
+            val currentNightMode =
+                resources.configuration.uiMode and android.content.res.Configuration.UI_MODE_NIGHT_MASK
+
+            // Изменение темы
+            when (currentNightMode) {
+                android.content.res.Configuration.UI_MODE_NIGHT_YES -> {
+                    // Если текущая тема - темная, то установить светлую тему
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+
+                android.content.res.Configuration.UI_MODE_NIGHT_NO -> {
+                    // Если текущая тема - светлая, то установить темную тему
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+            }
+
+            // Перезагрузка активити для применения изменений
+            recreate()
+        }
         second_btn.setOnClickListener { startActivity(Intent(this@Profile, Language::class.java)) }
         third_btn.setOnClickListener { startActivity(Intent(this@Profile, MyImage::class.java)) }
         fourth_btn.setOnClickListener { startActivity(Intent(this@Profile, Login::class.java)) }
